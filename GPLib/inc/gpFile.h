870da483a72d23cface4638005d39be9c717d045
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2007-11-20 22:37:46 +0000

    Start a generic file interface

---
+++ GPLib/inc/gpFile.h
@@ -0,0 +1,33 @@
+/* GPLib
+* Copyright (c) 2007 - 2008 Jeffrey Myers
+*
+* This package is free software;  you can redistribute it and/or
+* modify it under the terms of the license found in the file
+* named COPYING.txt that should have accompanied this file.
+*
+* THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#ifndef _GPFILE_H_
+#define _GPFILE_H_
+
+class GPFile
+{
+public:
+
+protected:
+  GPFile();
+  ~GPFile();
+};
+
+#endif //_GPFILE_H_
+
+// Local Variables: ***
+// mode:C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 2 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=2 tabstop=8
