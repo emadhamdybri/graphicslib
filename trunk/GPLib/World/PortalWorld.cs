39d33b6df92ee0e9a838c5f1e81a0bd1613bbf5f
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2009-09-13 18:59:50 +0000

    cleanups for new file locs update code to OpenTK .9.8

---
+++ trunk/GPLib/World/PortalWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Math;
+using Math3D;
+
+namespace World
+{
+    public class Portal
+    {
+        String destination = String.Empty;
+    }
+
+    public class PortalFace
+    {
+
+    }
+
+    public class PortalCell
+    {
+        String name = String.Empty;
+
+        public List<PortalFace> faces = new List<PortalFace>();
+        public List<Portal> portals = new List<Portal>();
+    }
+
+    class PortalWorld
+    {
+    }
+}
