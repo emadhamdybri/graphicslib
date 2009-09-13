468b8dcdaf7beb455c9aa89a1660743016df6b6a
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2009-09-13 21:03:57 +0000

    Add Keyboard and Mouse classes that work similar to the GameWindow ones.

---
+++ GPLib/GUIGameWindow/KeyboardDevice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text;
+
+namespace GUIGameWindow
+{
+    public delegate void KeyDownEvent(KeyboardDevice sender, Keys key);
+    public delegate void KeyUpEvent(KeyboardDevice sender, Keys key);
+   
+    public class KeyboardDevice
+    {
+        Dictionary<Keys, bool> keymap = new Dictionary<Keys,bool>();
+
+        public event KeyUpEvent KeyUp;
+        public event KeyDownEvent KeyDown;
+
+
+        public bool this[Keys key]
+        {
+            get 
+            {
+                if (!keymap.ContainsKey(key))
+                    return false;
+                else
+                    return keymap[key]; 
+            }
+        }
+
+        public KeyboardDevice(Form form)
+        {
+            form.KeyDown += new KeyEventHandler(InternalDown);
+            form.KeyUp += new KeyEventHandler(InternalUp);
+        }
+
+        protected void InternalDown(Object sender, KeyEventArgs args)
+        {
+            keymap[args.KeyCode] = true;
+            KeyDown(this, args.KeyCode);
+        }
+
+        protected void InternalUp(Object sender, KeyEventArgs args)
+        {
+            keymap[args.KeyCode] = false;
+            KeyUp(this, args.KeyCode);
+        }
+    }
+
+}
