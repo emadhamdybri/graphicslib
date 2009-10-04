e0bb5fea2a01c429cd0d721910f5f27912fe0a7e
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2009-10-04 07:46:57 +0000

    Common planes and verts

--- GPLib/Math/Utilities.cs
+++ GPLib/Math/Utilities.cs
@@ -262,7 +262,9 @@ namespace Math3D
 
     public class VectorHelper3
     {
-        public static float Distance ( Vector3 v1, Vector3 v2)
+        public static Vector3 Up = new Vector3(0, 0, 1);
+        public static Vector3 Down = new Vector3(0, 0, -1);
+        public static float Distance(Vector3 v1, Vector3 v2)
         {
             return (float)Math.Sqrt((v2.X - v1.X) * (v2.X - v1.X) + (v2.Y - v1.Y) * (v2.Y - v1.Y) + (v2.Z - v1.Z) * (v2.Z - v1.Z));
         }
