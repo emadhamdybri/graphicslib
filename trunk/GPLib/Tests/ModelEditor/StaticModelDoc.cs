1d7d35f3f5aa8c19a77fa908989c6eacc12002d9
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2009-10-21 16:17:50 +0000

    Add MDI model editor

---
+++ trunk/GPLib/Tests/ModelEditor/StaticModelDoc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace ModelEditor
+{
+    public partial class StaticModelDoc : ModelEditor.ModelBaseDoc
+    {
+        public StaticModelDoc()
+        {
+            InitializeComponent();
+        }
+
+        protected override void SetupView()
+        {
+            GL.ClearColor(Color.LightSlateGray);
+            GridSubDivColor = Color.DarkGray;
+        }
+    }
+}
