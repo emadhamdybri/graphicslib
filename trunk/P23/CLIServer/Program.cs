b9e2a1dfe382e2e47e6e1dd81d0436e916299417
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-01-06 21:07:55 +0000

    Add basic CLI server

---
+++ trunk/P23/CLIServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using Project23Server;
+
+namespace CLIServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 2501;
+            if (args.Length > 0)
+                port = int.Parse(args[0]);
+
+            Server server = new Server(port);
+
+            while (true)
+            {
+                server.Update();
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
