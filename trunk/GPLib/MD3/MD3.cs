b823171ac417413c35665a15e295894d9d5ee7dd
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-01-15 01:48:02 +0000

    MD3 reader

---
+++ trunk/GPLib/MD3/MD3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+
+namespace MD3
+{
+    public class FrameInfo
+    {
+        public String Name = string.Empty;
+        public Vector3 Max = Vector3.Zero;
+        public Vector3 Min = Vector3.Zero;
+        public Vector3 Origin = Vector3.Zero;
+        public float Radius = 0;
+    }
+
+    public class Component
+    {
+        public string Name
+        {
+            get { return name; }
+        }
+        string name = string.Empty;
+
+        public Component ( string n )
+        {
+            name = n;
+        }
+
+        public FrameInfo[] Frames;
+
+    }
+}
---
+++ trunk/GPLib/Tests/MD3ReaderTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using MD3;
+
+namespace MD3ReaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(args[0]);
+            Reader.ReadComponent(file);
+        }
+    }
+}
