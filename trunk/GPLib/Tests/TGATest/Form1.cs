cfaf2fedf7896f6a16ad3858b633e375f1b77137
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-01-17 20:56:12 +0000

    Add TGA test project

---
+++ trunk/GPLib/Tests/TGATest/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Drawables.TGA;
+
+namespace TGATest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pictureBox1.Image = TGAFile.Read("test32.tga");
+          //  this.pictureBox1.Image = TGAFile.Read("test8.tga");
+        }
+    }
+}
