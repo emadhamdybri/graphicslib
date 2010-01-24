a4940bbf4f1f880eda7bf6b2ccd4e1bc30c50f02
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-01-24 07:48:58 +0000

    Convert MD3 test to be graphical and draw the character

---
+++ trunk/GPLib/Tests/MD3ReaderTest/View.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MD3ReaderTest
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(609, 511);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            this.glControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseClick);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 515);
+            this.Controls.Add(this.glControl1);
+            this.Name = "View";
+            this.Text = "View";
+            this.Load += new System.EventHandler(this.View_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+    }
+}
---
+++ trunk/GPLib/Tests/MD3ReaderTest/View.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+using MD3;
+
+using OpenTK;
+using OpenTK.Graphics;
+
+using Drawables.DisplayLists;
+
+namespace MD3ReaderTest
+{
+    public partial class View : Form
+    {
+        Character character;
+        CharacterInstance instance;
+
+        Vector3 offset = new Vector3(0, 0, 5);
+        Vector2 rotation = new Vector2(0, 0);
+        float pullback = 100f;
+        Point lastMouse = Point.Empty;
+
+        int fps = 1;
+
+        float GridSubDivisions = 1.0f;
+        float GridSize = 50;
+        float GridStep = 2;
+
+        Timer frameTimer;
+
+        string dir = string.Empty;
+        public View(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length > 0)
+            {
+                character = Reader.Read(new DirectoryInfo(args[0]));
+                instance = new CharacterInstance(character);
+                dir = args[0];
+            }
+            glControl1.MouseWheel += new MouseEventHandler(glControl1_MouseWheel);
+
+            if (args.Length > 1)
+                instance.SetSkin(args[1]);
+
+            CharacterInstance.ProcessSurfacePath = new ProcessSurfacePathHandler(GetTexturePath);
+            CharacterInstance.DrawTags = true;
+        }
+
+        string GetTexturePath ( string path )
+        {
+            return Path.Combine(dir,Path.GetFileName(path));
+        }
+
+        void glControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            float zoomSpeed = 0.5f;
+            if (Control.ModifierKeys == Keys.Shift)
+                zoomSpeed *= 5f;
+
+            pullback += ((float)e.Delta / 120f) * zoomSpeed;
+            if (pullback < 0)
+                pullback = 0;
+            Render3dView();
+        }
+
+        private void glControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            float rotSpeed = 0.1f;
+            float moveSpeed = 0.002f;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                rotation.X -= (e.X - lastMouse.X) * rotSpeed;
+                rotation.Y += (e.Y - lastMouse.Y) * rotSpeed;
+            }
+
+            if (e.Button == MouseButtons.Middle)
+            {
+                float deg2Rad = (float)Math.PI / 180.0f;
+                float x = 0;
+                float y = 0;
+
+                float XDelta = (e.X - lastMouse.X) * moveSpeed * pullback;
+                float YDelta = (e.Y - lastMouse.Y) * moveSpeed * pullback;
+
+                x = (float)Math.Cos(rotation.X * deg2Rad) * XDelta;
+                y = -(float)Math.Sin(rotation.X * deg2Rad) * XDelta;
+
+                offset.X -= x;
+                offset.Y += y;
+
+
+                x = -(float)Math.Cos((rotation.X + 90) * deg2Rad) * YDelta;
+                y = (float)Math.Sin((rotation.X + 90) * deg2Rad) * YDelta;
+
+                offset.X -= x;
+                offset.Y += y;
+
+            }
+            Render3dView();
+
+            lastMouse = new Point(e.X, e.Y);
+        }
+
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void View_Load(object sender, EventArgs e)
+        {
+            SetupGL();
+
+            SetViewPort();
+            Render3dView();
+
+            frameTimer = new Timer();
+            frameTimer.Interval = 1000 / fps;
+            frameTimer.Tick += new EventHandler(frameTimer_Tick);
+            frameTimer.Start();
+        }
+
+        void frameTimer_Tick(object sender, EventArgs e)
+        {
+            if (instance != null)
+                instance.IncremntFullFrame();
+
+            Render3dView();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            SetViewPort();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Render3dView();
+        }
+
+        protected virtual void SetupGL()
+        {
+            GL.ClearColor(Color.SkyBlue);
+
+            GL.Enable(EnableCap.CullFace);
+            GL.CullFace(CullFaceMode.Back);
+            GL.FrontFace(FrontFaceDirection.Ccw);
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.PolygonMode(MaterialFace.Front, PolygonMode.Fill);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.Enable(EnableCap.DepthTest);
+
+            // setup light 0
+            Vector4 lightInfo = new Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+            GL.Light(LightName.Light0, LightParameter.Ambient, lightInfo);
+
+            lightInfo = new Vector4(0.7f, 0.7f, 0.7f, 1.0f);
+            GL.Light(LightName.Light0, LightParameter.Diffuse, lightInfo);
+            GL.Light(LightName.Light0, LightParameter.Specular, lightInfo);
+        }
+
+        protected virtual void SetViewPort()
+        {
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height); // Use all of the glControl painting area  
+
+            SetPerspective();
+        }
+
+        void SetPerspective()
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            float aspect = (float)glControl1.Width / (float)glControl1.Height;
+
+            Glu.Perspective(45, aspect, 1f, 1000f);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        void SetOrthographic()
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, glControl1.Width, 0, glControl1.Height, 0, 100);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        protected void SetCamera()
+        {
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Translate(0, 0, -pullback);								// pull back on allong the zoom vector
+            GL.Rotate(rotation.Y, 1.0f, 0.0f, 0.0f);					// pops us to the tilt
+            GL.Rotate(-rotation.X, 0.0f, 1.0f, 0.0f);					// gets us on our rot
+            GL.Translate(-offset.X, -offset.Z, offset.Y);	// take us to the pos
+            GL.Rotate(-90, 1.0f, 0.0f, 0.0f);							// gets us into XY
+        }
+
+        public void Setup3dView()
+        {
+            SetPerspective();
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            SetCamera();
+        }
+
+        protected void DrawGridAxisMarker()
+        {
+            GL.Disable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.Lighting);
+            GL.Disable(EnableCap.LineSmooth);
+
+            GL.LineWidth(2);
+            GL.Begin(BeginMode.Lines);
+
+            GL.Color3(Color.Blue);
+            GL.Vertex3(-1, 0, 0);
+            GL.Vertex3(2, 0, 0);
+
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, -1, 0);
+            GL.Vertex3(0, 2, 0);
+
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, -1);
+            GL.Vertex3(0, 0, 2);
+            GL.End();
+
+            GL.Enable(EnableCap.LineSmooth);
+            GL.PopMatrix();
+            GL.Enable(EnableCap.Lighting);
+            GL.DepthMask(true);
+        }
+
+        void GridList_Generate(object sender, DisplayList list)
+        {
+            GL.Disable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.Lighting);
+            GL.Enable(EnableCap.LineSmooth);
+            GL.DepthMask(false);
+
+            float gridSize = GridSize;
+
+            GL.PushMatrix();
+            GL.Translate(0, 0, 0);
+
+            GL.Color4(Color.FromArgb(128, Color.LightSlateGray));
+            GL.LineWidth(1);
+
+            if (GridSubDivisions > 0)
+            {
+                GL.Begin(BeginMode.Lines);
+
+                for (float i = 0; i < gridSize; i += GridSubDivisions)
+                {
+                //    if (i - (int)i < GridSubDivisions)
+                 //       continue;
+
+                    GL.Vertex2(gridSize, i);
+                    GL.Vertex2(-gridSize, i);
+
+                    GL.Vertex2(gridSize, -i);
+                    GL.Vertex2(-gridSize, -i);
+
+                    GL.Vertex2(i, gridSize);
+                    GL.Vertex2(i, -gridSize);
+
+                    GL.Vertex2(-i, gridSize);
+                    GL.Vertex2(-i, -gridSize);
+                }
+                GL.End();
+            }
+            GL.LineWidth(2);
+            GL.Begin(BeginMode.Lines);
+            GL.Color4(Color.FromArgb(128, Color.LightGray));
+
+            for (float i = 0; i < gridSize; i += GridStep)
+            {
+                GL.Vertex2(gridSize, i);
+                GL.Vertex2(-gridSize, i);
+
+                GL.Vertex2(gridSize, -i);
+                GL.Vertex2(-gridSize, -i);
+
+                GL.Vertex2(i, gridSize);
+                GL.Vertex2(i, -gridSize);
+
+                GL.Vertex2(-i, gridSize);
+                GL.Vertex2(-i, -gridSize);
+            }
+            GL.End();
+
+            GL.LineWidth(3);
+            GL.Begin(BeginMode.Lines);
+            GL.Color3(Color.Snow);
+            GL.Vertex3(-gridSize, 0, 0);
+            GL.Vertex3(gridSize, 0, 0);
+            GL.Vertex3(0, -gridSize, 0);
+            GL.Vertex3(0, gridSize, 0);
+            GL.End();
+
+            GL.Enable(EnableCap.LineSmooth);
+            GL.PopMatrix();
+            GL.Enable(EnableCap.Lighting);
+            GL.DepthMask(true);
+        }
+
+        public void Render3dView()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Setup3dView();
+            GL.PushMatrix();
+
+            GL.Enable(EnableCap.Light0);
+            Vector4 lightPos = new Vector4(10, 20, 20, 0);
+            GL.Light(LightName.Light0, LightParameter.Position, lightPos);
+            GridList_Generate(null, null);
+            DrawGridAxisMarker();
+
+            if (instance != null)
+                instance.Draw();
+            GL.PopMatrix();
+
+            glControl1.SwapBuffers();
+        }
+    }
+}
