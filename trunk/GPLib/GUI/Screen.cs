bcc03eae0e6ef7ec1038ff0a2de0c3e3a9df8501
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-02-01 07:38:04 +0000

    Start GUI lib to handle in game drawing

---
+++ trunk/GPLib/GUI/Screen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace GUI
+{
+    public class Screen : Item
+    {
+        public static float ScreenY = 0;
+
+        public static Font DefaultFont = new Font(FontFamily.GenericSerif, 10);
+        public static OpenTK.Graphics.TextPrinter Printer = new OpenTK.Graphics.TextPrinter();
+
+        public override void Resize(ResizeEventArgs args)
+        {
+            base.Resize(args);
+            Size = new Vector2(args.Size);
+            ScreenY = args.Size.Y;
+        }
+    }
+}
