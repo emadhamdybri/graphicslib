480b1d1be1ae3a2cc8edd001873df3c96a011bca
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-02-11 06:48:23 +0000

    add list server add and list

--- P2501/Services/List/list.php
+++ P2501/Services/List/list.php
@@ -1,5 +1,6 @@
 <?php
 	include_once('config.php');
+	include_once('../Common/db.php');
 	
 	function ListDBConnect()
 	{
@@ -9,12 +10,96 @@
 		global $config_listDB_database;
 
 		$db = mysql_pconnect($config_listDB_hostname,$config_listDB_username,$config_listDB_password);
-        if (!$db)
+		if (!$db)
 			die("ERROR: Unable to conenct to database.\n");
-        else
+		else
 			$result = mysql_select_db($config_listDB_database);
+
+		return $result;
+	}
+	
+	function ViewList()
+	{
+		$query = "SELECT ID, Hostname, ServerName, Description, Groupname FROM list";
+		$result = SQLGet($query);
+		
+		$count = mysql_num_rows($result);
+		echo $count . "\r\n";
+		for ($i = 0; $i < $count; $i += 1)
+		{
+			$row = mysql_fetch_array($result);
+			echo $row[1] ."\t". $row[2] ."\t". $row[3] ."\t". $row[4] ."\r\n";
+		}
+	}
+	
+	function GetInput ( $var )
+	{
+		 if (!array_key_exists($var,$_REQUEST))
+			 return FALSE;
+			 
+		return Sanitize($_REQUEST[$var]);
+	}
+	
+	function AddHost()
+	{
+		$host = GetInput("host");
+		$name = GetInput("name");
+		$description = GetInput("desc");
+		$group = GetInput("group");
+		
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$port = explode(":",$host);
+		if (count($port) > 1)
+			$port = $port[1];
+		else
+		{
+			$port = 2501;
+			$host = $host . ":2501";
+		}
+		
+		if (!$name)
+			$name = $host;
+			
+		if (!$group)
+			$group = "NULL";
+			
+		if (!$description)
+			$description = "NULL";
+			
+		if (!$host)
+		{
+			echo "err";
+			return;
+		}
 				
-		return $db;
+		$random = rand();
+		$now = gmdate("Y-m-d H:i:s", time());
+		
+		$query = "INSERT INTO list (Hostname, IP, HostPort, ServerName, Description, Groupname, UpdateTime) VALUES ('$host', '$ip', '$port', '$name', '$description', '$random', '$now')";
+		if (!SQLSet($query))
+		{
+				echo "dberr";
+				return;
+		}
+		
+		$id = GetDBFieldForKey ( "Groupname", $random, "list", "ID" );
+		if (!$id)
+		{
+				echo "dberr";
+				return;
+		}
+		
+		 SetDBFieldForKey("ID", $id, "list", "Groupname", $group);
+		 
+		 echo $id;
+	}
+	
+	function 	UpdateHost()
+	{
+	}
+	
+	function RemoveHost()
+	{
 	}
 	
 	header('Content-Type: text/plain');
@@ -37,5 +122,4 @@
 		UpdateHost();
 	else if ($action == 'removehost')
 		RemoveHost();
-
 ?>
