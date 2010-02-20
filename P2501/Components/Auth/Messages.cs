14a6cdb24495611c4dab83c5d6b835247600d348
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-02-20 23:53:00 +0000

    Add authentication lib for crypto communication

---
+++ P2501/Components/Auth/Messages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+
+namespace Auth
+{
+    public class RequestAdd : AuthMessage
+    {
+        public string email = string.Empty;
+        public string password = string.Empty;
+        public string callsign = string.Empty;
+
+        public RequestAdd()
+        {
+            Name = AuthMessage.RequestAdd;
+        }
+
+        public override NetBuffer Pack()
+        {
+            NetBuffer buffer = base.Pack();
+            buffer.Write(email);
+            buffer.Write(password);
+            buffer.Write(callsign);
+            return buffer;
+        }
+
+        public override bool Unpack(ref NetBuffer buffer)
+        {
+            if (!base.Unpack(ref buffer))
+                return false;
+
+            email = buffer.ReadString();
+            password = buffer.ReadString();
+            callsign = buffer.ReadString();
+            return true;
+        }
+    }
+}
