d5460ff0164ae520cc5b939c47f30d81773977a9
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-02-20 20:05:22 +0000

    Start client with forms for login and registration

---
+++ P2501/Apps/Client/RegistrationForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class RegistrationForm : Form
+    {
+        public string AccountName = string.Empty;
+
+        public RegistrationForm()
+        {
+            InitializeComponent();
+            TermsBrowser.Visible = false;
+            Check.Enabled = false;
+            OK.Enabled = false;
+        }
+
+        private void Terms_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (TermsBrowser.Visible)
+                return;
+
+            TermsBrowser.Visible = true;
+            TermsBrowser.Navigate("http://www.awesomelaser.com/p2501/terms.html");
+            this.Height += 250;
+        }
+
+        private void CheckOK ()
+        {
+            OK.Enabled = false;
+
+            if (Email.Text == string.Empty || !Email.Text.Contains('@'))
+                return;
+
+            if (Password.Text == string.Empty || PassVerify.Text == string.Empty || Password.Text != PassVerify.Text)
+                return;
+
+            if (Callsign.Text == string.Empty)
+                return;
+
+            if (!Agree.Checked)
+                return;
+
+            OK.Enabled = true;
+        }
+
+        private void CheckPasswords()
+        {
+            PassError.Visible = Password.Text != PassVerify.Text;
+        }
+        private void Email_TextChanged(object sender, EventArgs e)
+        {
+            CheckOK();
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            CheckOK();
+            CheckPasswords();
+        }
+
+        private void PassVerify_TextChanged(object sender, EventArgs e)
+        {
+            CheckOK();
+            CheckPasswords();
+        }
+
+        private void Callsign_TextChanged(object sender, EventArgs e)
+        {
+            CheckOK();
+            Check.Enabled = Callsign.Text != string.Empty;
+        }
+
+        private void Agree_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckOK();
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            // check availability
+            if (false)
+            {
+                MessageBox.Show("The name " + Callsign.Text + " is not available");
+                Callsign.Text = string.Empty;
+                Callsign.Select();
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            Check_Click(sender,e);
+            if (Callsign.Text == string.Empty)
+            {
+                DialogResult = DialogResult.None;
+                return;
+            }
+
+            AccountName = Email.Text;
+        }
+    }
+}
---
+++ P2501/Apps/Client/StartupForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class StartupForm : Form
+    {
+        bool logedIn = false;
+
+        public StartupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartupForm_Load(object sender, EventArgs e)
+        {
+            NewsBrowser.Navigate("http://www.awesomelaser.com/p2501/news.html");
+
+            UpdateUIStates();
+        }
+
+        protected void UpdateUIStates ()
+        {
+            CallsignsGroup.Enabled = logedIn;
+            GamesGroup.Enabled = logedIn;
+
+            Password.Enabled = Username.Text != string.Empty;
+
+            LoginButton.Enabled = Username.Text != string.Empty && Password.Text != string.Empty && Password.Enabled;
+        }
+
+        private void Username_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateUIStates();
+        }
+
+        private void Username_TextUpdate(object sender, EventArgs e)
+        {
+            UpdateUIStates();
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            UpdateUIStates();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            RegistrationForm form = new RegistrationForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                Username.Text = form.AccountName;
+            }
+            UpdateUIStates();
+        }
+    }
+}
