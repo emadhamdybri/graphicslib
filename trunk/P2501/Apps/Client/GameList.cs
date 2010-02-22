54d04d6a32c012b95178de093ccbdea209923e7a
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-02-22 03:18:09 +0000

    Add classes for game list and login services

---
+++ trunk/P2501/Apps/Client/GameList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace P2501Client
+{
+    class GameList : IDisposable
+    {
+        UInt64 UID = 0;
+
+        List<string> GameServers = new List<string>();
+        bool dirty = false;
+
+        Thread worker = null;
+
+        public bool Dirty
+        {
+            get
+            {
+                lock (GameServers)
+                {
+                    return dirty; 
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Kill();
+        }
+
+        public void Kill ()
+        {
+            if (worker == null)
+                return;
+
+            lock (GameServers)
+            {
+                worker.Abort();
+                worker = null;
+            }
+        }
+
+        public GameList ( UInt64 id )
+        {
+            UID = id;
+
+            worker = new Thread(new ThreadStart(Poll));
+            worker.Start();
+        }
+
+        void Poll ()
+        {
+            lock (GameServers)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.awesomelaser.com/p2501/List/list.php?action=viewlist&uid=" + UID.ToString());
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                GameServers.Clear();
+
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+
+                int count = int.Parse(reader.ReadLine());
+                for (int i = 0; i < count; i++)
+                    GameServers.Add(reader.ReadLine());
+
+                dirty = true;
+            }
+
+            Thread.Sleep(120 * 1000);
+        }
+
+        public List<string> GetGameServers ()
+        {
+            List<string> newList = new List<string>();
+            lock(GameServers)
+            {
+                foreach (string g in GameServers)
+                    newList.Add(g);
+               
+                dirty = false;
+            }
+
+            return newList;
+        }
+    }
+}
---
+++ trunk/P2501/Apps/Client/Login.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text;
+
+using System.Diagnostics;
+using System.Threading;
+
+using Clients;
+using Lidgren.Network;
+
+using Auth;
+
+namespace P2501Client
+{
+    class Login
+    {
+        public UInt64 UID = 0;
+        public UInt64 Token = 0;
+
+        CryptoClient client = null; 
+
+        public bool Connect (string email, string password )
+        {
+            WaitBox box = new WaitBox("Logon");
+            box.Show();
+            box.Update(10, "Contacting secure host");
+
+            if (client != null)
+                client.Kill();
+
+            // CryptoClient client = new CryptoClient("www.awesomelaser.com", 4111);
+            client = new CryptoClient("localhost", 4111);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            int timeout = 120;
+
+            bool done = false;
+            bool connected = false;
+
+            while (!done)
+            {
+                if (!connected && client.IsConnected)
+                    box.Update(20, "Connection established");
+                if (connected && !client.IsConnected)
+                    done = true;
+
+                if (!done)
+                {
+                    NetBuffer buffer = client.GetPentMessage();
+                    while (buffer != null)
+                    {
+                        int name = buffer.ReadInt32();
+
+                        if (name == AuthMessage.Hail)
+                        {
+                            RequestAuth msg = new RequestAuth();
+                            msg.email = email;
+                            msg.password = password;
+
+                            box.Update(75, "Sending credentials");
+                            client.SendMessage(msg.Pack(), msg.Channel());
+                        }
+                        else if (name == AuthMessage.AuthOK)
+                        {
+                            AuthOK ok = new AuthOK();
+                            ok.Unpack(ref buffer);
+                            box.Update(100, "Login complete");
+                            box.Close();
+                            UID = ok.ID;
+                            Token = ok.Token;
+                            return true;
+                        }
+                        else if (name == AuthMessage.AuthBadCred)
+                        {
+                            client.Kill();
+                            client = null;
+                            box.Close();
+                            MessageBox.Show("Login Failed");
+                            return false;
+                        }
+                        else
+                        {
+                            done = true;
+                            connected = false;
+                        }
+
+                        if (!done)
+                            buffer = client.GetPentMessage();
+                        else
+                            buffer = null;
+                    }
+
+                    if (timer.ElapsedMilliseconds / 1000 > timeout)
+                    {
+                        done = true;
+                        connected = false;
+                    }
+                    Application.DoEvents();
+                    Thread.Sleep(100);
+                }
+            }
+            box.Close();
+
+            client.Kill();
+            client = null;
+            MessageBox.Show("The login server could not be contacted");
+
+            return false;
+        }
+
+        public Dictionary<UInt64,string> GetCharacterList ()
+        {
+            if (client == null)
+                return null;
+
+            WaitBox box = new WaitBox("Callsigns");
+            box.Update(20, "Requesting list");
+            NetBuffer buffer = new NetBuffer();
+            buffer.Write(AuthMessage.RequestCharacterList);
+            client.SendMessage(buffer, NetChannel.ReliableInOrder1);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            int timeout = 30;
+
+            while (timer.ElapsedMilliseconds / 1000 < timeout)
+            {
+                buffer = client.GetPentMessage();
+                while (buffer != null)
+                {
+                    int name = buffer.ReadInt32();
+
+                    if (name == AuthMessage.CharacterList)
+                    {
+                        Dictionary<UInt64, string> list = new Dictionary<UInt64, string>();
+
+                        CharacterList cList = new CharacterList();
+                        cList.Unpack(ref buffer);
+
+                        foreach (CharacterList.CharacterInfo info in cList.Characters)
+                            list.Add(info.CID, info.Name);
+
+                        client.Kill();
+                        client = null;
+                        box.Update(100, "List Complete");
+                        box.Close();
+                        return list;
+                    }
+                    buffer = client.GetPentMessage();
+                } 
+                Application.DoEvents();
+                Thread.Sleep(100);
+            }
+            box.Close();
+            client.Kill();
+            client = null;
+            return null;      
+        }
+    }
+}
