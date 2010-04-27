d7d968d5a86bb5451cdc7f8a4809e741477413e3
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-04-27 00:43:21 +0000

    simple game to test engine systems.

---
+++ trunk/netwarriors/netwar/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace netwar
+{
+    public partial class Startup : Form
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
