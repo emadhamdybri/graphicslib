82611e914373e56d5fd2b7b17a17eeb0dd969d12
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-05-01 00:53:48 +0000

    Add simpler test app

---
+++ trunk/kingsreign/KingsReign/GameWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KingsReign
+{
+    public partial class GameWindow : Form
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
