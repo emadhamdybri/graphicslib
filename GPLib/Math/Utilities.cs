d05502d0b221f4d149d0415e4f5a65cbb78a9118
JeffM2501 <JeffM2501@e1b1b32a-243e-0410-ac30-ad2a981e1070>
2010-05-12 01:39:02 +0000

    FromAngle with a radius

--- GPLib/Math/Utilities.cs
+++ GPLib/Math/Utilities.cs
@@ -509,5 +509,10 @@ namespace Math3D
         {
             return new Vector2((float)Math.Cos(Trig.DegreeToRadian(angle)), (float)Math.Sin(Trig.DegreeToRadian(angle)));
         }
+
+        public static Vector2 FromAngle(float angle, float radius)
+        {
+            return new Vector2((float)Math.Cos(Trig.DegreeToRadian(angle)) * radius, (float)Math.Sin(Trig.DegreeToRadian(angle))*radius);
+        }
     }
 }
